7635ef78329ac15202c51dbd1bb75893fa38b099
Timmythepirate <geoffgates@yahoo.com>
2016-04-06 22:06:59 -0500

    change all tabs to spaces

--- sps/treePage.controller.js
+++ sps/treePage.controller.js
@@ -5,9 +5,9 @@ sap.ui.controller("sps.treePage", {
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 * @memberOf sps.sps
 */
-//	onInit: function() {
+//  onInit: function() {
 //
-//	},
+//  },
     goHome: function() {
         app.back();
     }
@@ -16,25 +16,25 @@ sap.ui.controller("sps.treePage", {
 * (NOT before the first rendering! onInit() is used for that one!).
 * @memberOf sps.sps
 */
-//	onBeforeRendering: function() {
+//  onBeforeRendering: function() {
 //
-//	},
+//  },
 
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
 * @memberOf sps.sps
 */
-//	onAfterRendering: function() {
+//  onAfterRendering: function() {
 //
-//	},
+//  },
 
 /**
 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
 * @memberOf sps.sps
 */
-//	onExit: function() {
+//  onExit: function() {
 //
-//	}
+//  }
 
 });
