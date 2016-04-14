fc5e981adcb61cd0581ff9afb0f6d64bceaaa10b
Timmythepirate <geoffgates@yahoo.com>
2016-04-13 19:28:48 -0500

    sync branch with master

--- sps/treePage.controller.js
+++ sps/treePage.controller.js
@@ -5,45 +5,23 @@ sap.ui.controller("sps.treePage", {
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 * @memberOf sps.sps
 */
-  currentNode : 0,
-  nodeChoice : 0,
   onInit: function() {
-      var oModel = new sap.ui.model.json.JSONModel();
-      oModel.loadData("sps/questions.json");
-      sap.ui.getCore().setModel(oModel);
+    var oModel = new sap.ui.model.json.JSONModel();
+    oModel.loadData("sps/questions.json");
+    sap.ui.getCore().setModel(oModel);
+    //set the text property of the "quest" to "q1"
+    this.getView().byId("quest").bindText("/Questions/q1");
   },
   goHome: function() {
-        app.back();
-  },
-  getCurrentNode : function() {
-	  return currentNode;
+    app.back();
   },
   respondYes : function() {
-	  nodeChoice = 1;
-	  masterFunction();
-	  //sap.ui.getCore().refresh()
+    // change the question to Question #2
+    this.getView().byId("quest").bindText("/Questions/q2");
   },
   respondNo : function() {
-	  nodeChoice = 2;
-	  masterFunction();
-  },
-  masterFunction : function() {
-	  if (currentNode == 0) {
-		  //this should'nt happen
-	  }
-	  if (currentNode == 1){
-
-	  }
-	  if (currentNode == 2){
-
-	  }
-	  if (currentNode == 3){
-
-	  }
-	  if (currentNode == 4){
-
-	  }
-  },
+    this.getView().byId("quest").bindText("/Questions/q3");
+  }
 
 
 /**
@@ -51,35 +29,28 @@ sap.ui.controller("sps.treePage", {
 * (NOT before the first rendering! onInit() is used for that one!).
 * @memberOf sps.sps
 */
-//  onBeforeRendering: function() {
-//
-//  },
+/*
+ onBeforeRendering: function() {
 
+  }
+*/
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
 * @memberOf sps.sps
 */
+
 /*
 onAfterRendering: function() {
-	  if (currentNode == 0) {
-		  //this should'nt happen
-		  alert("Test");
-	  }
-	  if (currentNode == 1){
 
-	  }
-	  if (currentNode == 2){
-		  app.to(treePage);
-	  }
-	  if (currentNode == 3){
-
-	  }
-	  if (currentNode == 4){
-
-	  }
 }
 */
+
+//  onAfterRendering: function() {
+//
+//  },
+
+
 /**
 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
 * @memberOf sps.sps
