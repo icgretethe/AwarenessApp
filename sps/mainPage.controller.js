d4304fece0daf30a46922a04359dc83957547346
Geoff Gates <geoffgates@yahoo.com>
2016-04-15 14:38:47 -0500

    make style consistent and add comments

--- sps/mainPage.controller.js
+++ sps/mainPage.controller.js
@@ -1,48 +1,14 @@
 sap.ui.controller("sps.mainPage", {
-
-/**
-* Called when a controller is instantiated and its View controls (if available) are already created.
-* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf sps.sps
-*/
-//  onInit: function() {
-//
-//  },
-goToTree: function() {
-    app.to(treePage);
-},
-
-toInfoPage: function() {
-    app.to(infoPage);
-},
-
-toCareReport: function() {
-    sap.m.URLHelper.redirect("https://publicdocs.maxient.com/reportingform.php?SIUEdwardsville&layout_id=2", false);
-}
-/**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf sps.sps
-*/
-//  onBeforeRendering: function() {
-//
-//  },
-
-/**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf sps.sps
-*/
-//  onAfterRendering: function() {
-//
-//  },
-
-/**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf sps.sps
-*/
-//  onExit: function() {
-//
-//  }
-
+    // go to the Decision Tree Page
+    goToTree: function() {
+        app.to(treePage);
+    },
+    // go to Info Page
+    toInfoPage: function() {
+        app.to(infoPage);
+    },
+    // leave the app and go to the Patient Care Report
+    toCareReport: function() {
+        sap.m.URLHelper.redirect("https://publicdocs.maxient.com/reportingform.php?SIUEdwardsville&layout_id=2", false);
+    }
 });
