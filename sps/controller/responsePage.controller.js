31539cf6f9f8e0339bba97a1398bc8ece87a1f02
Timmythepirate <geoffgates@yahoo.com>
2016-08-29 20:22:29 -0500

    update files that had slight content changes

--- sps/controller/responsePage.controller.js
+++ sps/controller/responsePage.controller.js
@@ -1,12 +1,12 @@
 sap.ui.controller("sps.controller.responsePage", {
 
     onInit: function() {
-        // create JSON model to manage data
-        var oModel = new sap.ui.model.json.JSONModel();
-        // load data from JSOM file, for use by the model
-        oModel.loadData("sps/responses.json");
-        sap.ui.getCore().setModel(oModel);
 
+        // create JSON model to manage data
+        //var oModel = new sap.ui.model.json.JSONModel();
+        // load data from JSOM file, for use by the model
+        //oModel.loadData("sps/responses.json");
+        //sap.ui.getCore().setModel(oMdl);
         switch(responseCounter) {
             case 1:
                 this.getView().byId("resp").bindText("/Responses/r1");
@@ -14,15 +14,23 @@ sap.ui.controller("sps.controller.responsePage", {
             case 2:
                 this.getView().byId("resp").bindText("/Responses/r2");
                 break;
+            case 3:
+                this.getView().byId("resp").bindText("/Responses/r3");
+                break;
+            case 4:
+                this.getView().byId("resp").bindText("/Responses/r4");
+                break;
             default:
-                // this.getView().byId("resp").bindText("/Responses/r1");
+                this.getView().byId("resp").bindText("/Error/errR");
                 break;
         }
     },
 
     // return to main page
     goHome: function() {
-        app.back();
+        questionCounter = 1;
+        responseCounter = 1;
+        app.to(mainPage);
     }
 
 });
