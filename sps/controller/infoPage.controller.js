f67e3a49f1f113d585898137c9c744cdc7c2ed71
jdonova <jdonova@siue.edu>
2016-09-22 20:39:27 -0500

    Second attempt at contacts

--- sps/controller/infoPage.controller.js
+++ sps/controller/infoPage.controller.js
@@ -1,18 +1,41 @@
 // when the back button is pressed, person returns to main page
 sap.ui.controller("sps.controller.infoPage", {
 	onInit: function() {
-        responseCounter = 1;
-        // create JSON model to manage data
-        //var oModel = new sap.ui.model.json.JSONModel();
-        oModel = new sap.ui.model.json.JSONModel();
-        // load data from JSOM file, for use by the model
-        oModel.loadData("sps/imporantContacts.json");
-        sap.ui.getCore().setModel(oModel);
+		responseCounter = 1;
+		// create JSON model to manage data
+		//var oModel = new sap.ui.model.json.JSONModel();
+		oModel = new sap.ui.model.json.JSONModel();
+		// load data from JSOM file, for use by the model
+		oModel.loadData("sps/imporantContacts.json");
+		sap.ui.getCore().setModel(oModel);
 
-        // set the text property of the Question to "q1", when page is first loaded
-        this.getView().byId("quest").bindText("/Contacts/1");
-    },
+		var oTable1 = new sap.ui.table.Table({
+			title : "Contacts"
+		})
+		oTable1.addColumn(new sap.ui.table.Column({
+			label : new sap.ui.commons.Label({
+				text : "Imporant Contacts"
+			}),
+			template : new sap.ui.commons.TextView({
+				text: '{contact}'
+			}),
+			width : "10px"
+		}));
+		oTable1.placeAt('content');
+	}),
+		
     goHome: function() {
         app.back();
     }
 });
+
+
+oTable1.addColumn(new sap.ui.table.Column({  
+   label : new sap.ui.commons.Label({  
+      text : "Player Name"  
+   }),  
+   template : new sap.ui.commons.TextView({
+      text: '{name}'
+   }),
+   width : "10px"  
+}));
