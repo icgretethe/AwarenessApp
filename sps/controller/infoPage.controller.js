c009e19ecaf81d11929049e8ca742c3b3c2ebae6
jdonova <jdonova@siue.edu>
2016-09-27 21:15:12 -0500

    Add Important contacts and locations list to json file. Need to be styled still.

--- sps/controller/infoPage.controller.js
+++ sps/controller/infoPage.controller.js
@@ -1,5 +1,18 @@
 // when the back button is pressed, person returns to main page
-sap.ui.controller("sps.controller.infoPage", {		
+sap.ui.controller("sps.controller.infoPage", {
+
+	onInit: function() {
+		// create JSON model to manage data
+		//JSON model was created in treePage, edit future
+		var contactList = this.getView().byId("list");
+		contactList.bindItems({
+			path: "/Contacts",
+			template: new sap.m.ObjectListItem({
+				attributes: [new sap.m.ObjectAttribute({
+				text: "{contact} "})]
+			})
+		})
+	},
     goHome: function() {
         app.back();
     }
