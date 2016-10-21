3a9acdaeeae02cd6df03af5b78ab0b39e5924c6f
jdonova <jdonova@siue.edu>
2016-10-20 19:33:05 -0500

    Update infoPage.controller.js

--- sps/controller/infoPage.controller.js
+++ sps/controller/infoPage.controller.js
@@ -10,7 +10,7 @@ sap.ui.controller("sps.controller.infoPage", {
 			template: new sap.m.ObjectListItem({
 				type: "Active",
 				tap: function() {
-					sap.m.URLHelper.triggerTel("{phone}")
+					sap.m.URLHelper.triggerTel("3143884445")
 				},
 				attributes: [new sap.m.ObjectAttribute({
 				text: "{text}",
